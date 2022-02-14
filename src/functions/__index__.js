04d33b1cab1047284b8065dc742223eb30bead89
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-02-14 12:38:36 -0500

    Update __index__.js

--- src/functions/__index__.js
+++ src/functions/__index__.js
@@ -6,26 +6,38 @@ const functions = {
 
    (!name || !key) ? (return) : null;
 
-   const \name = { ... };
+   const arguments[0] = { ... };
 
-   \name[key.name] = key.value;
-   await \name[key];
-   \name.slice(\name.indexOf(key.name));
+   arguments[0][key.name] = key.value;
+   await arguments[0][key.name];
+   arguments[0].slice(arguments[0].indexOf(key.name));
 
  },
  resources:() => {
+   const Resources = new Javah.data({});
 
    packageMarket:() => {
+     for(x = 0;x < Resources.total();x++) {
+        var packages = [];
+        
+        if(Resources[x].type === "package") {
+          const PackageInstaller = Javah.packageIntaller({ cache: true })
 
+          PackageInstaller.install(Resources[x], packages);
+        }
+     }
+
+     return packages;
    }
 
+   return Resources;
  },
- start:(buildName) => {
+ start:(build) => {
 
-   (!buildName) ? throw new Error("Nothing to start.") : null;
+   (!build) ? throw new Error("Nothing to start.") : null;
 
    while(callback()) { return this };
-   continue();
+   continue(x);
 
  }
 }
