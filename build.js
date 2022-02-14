fc16e3837d19ac76a4316cc59d68f5f909fb9f03
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-02-14 03:36:41 -0500

    Update build.js

--- build.js
+++ build.js
@@ -7,11 +7,11 @@ const build = x:packageMarket();
 
 for(x2 = 0; x2 < packages.total(); x2++) {
 
-*//
+/**
  @Module { javah }
  @Javah { for } { i=index.create(x), __packages.package=x[i] }
  @PackageOptimizer { package }
-/*
+*/
 
 x:queue("build", packages[x2]);
 
