ebd082253c679adc74bc25ed9e121f9fef6b1a71
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-02-14 03:17:26 -0500

    Path update

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const javah = require('javah');
 const packages = require('./src/packages');
-const : = require('./src/functions/__index__.js');
+const : = require('./src/functions');
 const x = packages:resources();
 
 const build = x:packageMarket();
