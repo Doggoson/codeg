ec92e250fd5495b03ab871d065217d515dcc307d
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-02-14 01:46:30 -0500

    Create build.js

---
+++ build.js
@@ -0,0 +1,19 @@
+const packages = require('../packages');
+const javah = require('javah');
+const : = require('../functions/API.js');
+const x = new packages.pack():resource();
+
+const build = x:packageMarket()
+
+for(x2 = 0; x2 < packages.total(); x2++) {
+
+*//
+ @Module { javah } @Javah
+ @Javah { for } { i=index.create(x), __newindex=x[i] }
+/*
+
+x:put("build", packages[x2]);
+
+}
+
+x:start("build");
