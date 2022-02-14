9e0726c729f5ea7bec2b5c2be8a702286b192bed
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-02-14 03:34:03 -0500

    Create __index__.js

---
+++ src/functions/__index__.js
@@ -0,0 +1,34 @@
+const functions = {
+ *//
+ @param { Javah } javah
+ /*
+ queue:(name, key) => {
+
+   (!name || !key) ? (return) : null;
+
+   const \name = { ... };
+
+   \name[key.name] = key.value;
+   await \name[key];
+   \name.slice(\name.indexOf(key.name));
+
+ },
+ resources:() => {
+
+   packageMarket:() => {
+
+   }
+
+ },
+ start:(buildName) => {
+
+   (!buildName) ? throw new Error("Nothing to start.") : null;
+
+   while(callback()) { return this };
+   continue();
+
+ }
+}
+
+module.exports = functions;
+
