2ec6087a5026d7b0377b04cfea13830ca2a5da1f
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-02-15 18:31:08 -0500

    Update client.js

--- src/functions/windows/client.js
+++ src/functions/windows/client.js
@@ -1,10 +1,16 @@
 const UI = require('@GithubLibraries/UI');
 const Expressified = require('@GithubLibraries/Expressified')({ routes: require("../routes") });
 
+// UI Library doesn't load. \\
 if(!UI) return Expressified.routes.post("client", "UI failed to load.");
+
 const Window = new UILibrary.window({ resolution: this.aspect.fit });
 
 const Dashboard = new Window.items()
-      .addItem({ type: this.button, row: this.rows.bottom, column: this.columns.bottom }, {});
+      .addItem(
+        { type: this.button, row: this.rows.bottom, column: this.columns.bottom }, 
+        { text: String, color: "Hexadecimal" },
+        function clicked() {  }
+      );
 
 Expressified.routes.update("client", Dashboard);
