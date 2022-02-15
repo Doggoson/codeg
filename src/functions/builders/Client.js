6c46b0d511f50dc89b3ab1596330d2084c0f2890
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-02-14 19:26:28 -0500

    Update Client.js

--- src/functions/builders/Client.js
+++ src/functions/builders/Client.js
@@ -1,1 +1,7 @@
+const UILibrary = require('@GithubLibraries/UI');
+const Window = new UILibrary.window({ resolution: "1024x600" });
 
+const Dashboard = new Window.items()
+      .addItem({ type: this.button, row: this.rows.bottom, column: this.columns.bottom }, {})
+
+if(!UILibrary) console.log('UI failed to load.');
