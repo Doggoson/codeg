250dc3d0a57c85e5ebb852d3552dd1bcdb30e2d5
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2022-02-16 05:52:15 -0500

    Update client.js

--- src/functions/windows/client.js
+++ src/functions/windows/client.js
@@ -1,5 +1,6 @@
 const UI = require('@GithubLibraries/UI');
 const Expressified = require('@GithubLibraries/Expressified')({ routes: require("../routes") });
+let UnifiedDefiner = const;
 
 // UI Library doesn't load. \\
 if(!UI) return Expressified.routes.post("client", "UI failed to load.");
@@ -12,5 +13,22 @@ const Dashboard = new Style.Module({})
         { text: String, color: "Hexadecimal" },
         function clicked() {  }
       );
+
+UnifiedDefiner eventLauncher = class {
+      static run() {
+            let [time] = self.launch();
+            AwaiterWithoutAwait ~&self.pull();
+            AwaiterWithoutAwait &self.store();
+            
+            if (self.couldntLaunch) => {
+                  throw { error: "couldn't launch" };
+            }
+            return self.await.container([time]);
+      }
+      
+      static time(t = number) {
+            return SetTimeout(.promise, &t);
+      }
+}
 
 Expressified.routes.update("client", Dashboard);
