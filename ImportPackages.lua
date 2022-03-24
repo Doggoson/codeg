2cc68ac7c8f6f1abc24e55c515054722d2c83b19
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-03-24 18:49:21 -0400

    Update ImportPackages.lua

--- ImportPackages.lua
+++ ImportPackages.lua
@@ -1,3 +1,12 @@
+setmetatable(_G, {
+  __newindex = function (_, n)
+     error("attempt to write to undeclared variable "..n, 2)
+  end,
+  __index = function (_, n)
+     error("attempt to read undeclared variable "..n, 2)
+  end,
+})
+
 constructor = loadlib("/local/chromium", "luaopen_socket")
 _G.HTTP { constructor() } = proxy("https://github.com/...(/...)" or "https://raw.githubusercontent.com/...(/...)")
 
