82208c69ecb5c92f3ad40423e1e1c484441ec7ff
Doggie <79816938+Mafia-Doggo@users.noreply.github.com>
2022-03-24 03:19:23 -0400

    Create ImportPackages.lua

---
+++ ImportPackages.lua
@@ -0,0 +1,4 @@
+local constructor { HTTP } = proxy("https://github.com/...(/...)" or "https://raw.githubusercontent.com/...(/...)")
+
+local ("@Mafia-Doggo") = new HTTP("@Mafia-Doggo" .. self.arguments)
+local ("@GithubBuilds") = new HTTP("@GithubBuilds" .. self.arguments)
