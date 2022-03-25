68be4897ed24bb2633ff74192ecd7b3afcd7c7ff
Doggie <79816938+Doggie-lab@users.noreply.github.com>
2022-03-25 17:54:11 -0400

    Update Attributes.js

--- Attributes.js
+++ Attributes.js
@@ -1,5 +1,5 @@
 const Library = require('@Mafia-Doggo/Attributes-Library');
-const Convert = require('@GithubBuilds/Lang-Convert');
+const Convert = require('@GithubBuilds/Language-Convert/Convert.c');
 
 for(f = 0; f < Library.Attributes.length; require(Library.Attributes)[f++]) {
   (f.Name)(f.exports.arguments) = (Convert(f.Source));
