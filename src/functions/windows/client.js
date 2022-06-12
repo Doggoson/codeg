f3948b18de6c519a3b34f10b4cd9ee2a45612421
Doggie <79816938+Doggoson@users.noreply.github.com>
2022-06-12 12:30:06 -0400

    Update client.js

--- src/functions/windows/client.js
+++ src/functions/windows/client.js
@@ -3,49 +3,30 @@ const $RE = require('re');
 const Expressified = require('@GithubLibraries/Expressified')({ routes: require($RE.dir) });
 const Javah = require('javah');
 
-// UI Library doesn't load. \\
-if(!UI) return Expressified.routes.update("client", "Page failed to load.");
+if(!UI) return Expressified[user].routes.open("client", "Page failed to load.");
 
-const Style = new UI.Style({});
+const Library = new UI();
 
-const Dashboard = new Style.Style({})
+const Dashboard = new Library.Style()
       .body(
         { margin: 0px, padding: 0px, background_color: "Hexadecimal" }
       )
       .addItem(
         { type: this.button, row: this.rows.bottom, column: this.columns.bottom, width: 10%, height: 50px }, 
-        { text: "", color: "Hexadecimal" },
-        clicked(Position) => {  }
+        { text: " ", color: Hexadecimal },
+        (Position) => {  }
       );
 
-function(__index, __newindex) = class {
-      // Create Event Collector. \\
-      this.event = new $RE.EventCollector();
-      // Create User Collector. \\
-      this.user = new $RE.UserCollector();
-      // Events are looped. \\
-      static run() {
-            var time = new Data.getTime() - self.launch();
-            // Pull the event data. \\
-            AwaiterWithoutAwait ~& self.pull();
-            // Store the event data. \\
-            AwaiterWithoutAwait &self.store();
-            // Then backup the event data for later use. \\
-            self.backup();
+
+ const Event = new $RE.EventParser()
+ const User = new $RE.UserParser()
+
+ const Time = new Data.getTime() - this.launched()
+ const Data = (this.pull() == null) ? this.store() : Data
+ const BackupData = this.backup()
             
-            if(self.installedVersion !== self.latestVersion) return Expressified[user].routes.update("client", "Client out of date, Please install new files.");
-            if(self.launchFailed) throw new Error("Client failed to launch.");
-      }
+ if(app.installedVersion !== app.latestVersion) return Expressified[user].routes.open("clientError", "Client out of date, Relaunch the app.")
+ if(app.launchFailed) return Expressified[user].routes.open("clientError", "Client failed to launch.")
 
-     // Event Handler. \\
-     this.event.called((name, source) => {
-           this.event.runFunction(() => {
-                 var event = new Event(name, source);
-                 
-                 if(event.callback()) return Expressified[user].routes.update("client", `Failed to run event: ${name}`)
-           })
-     })
-}
+ Expressified[user].routes.open("client", Dashboard);
 
-//Server Sided / Permanent Route Update\\
-Expressified.routes.update("client", Dashboard);
