d4edf737b987189595a6509083c09d77f3b4d221
Doggie <79816938+Doggoson@users.noreply.github.com>
2022-06-25 06:16:26 -0400

    Update PlayerV2.c

--- src/defaultCache/PlayerV2.c
+++ src/defaultCache/PlayerV2.c
@@ -2,17 +2,31 @@
 
 class VideoPlayer {
   public:
-    bool Playing;
-    int* ptr VideoCode;
+    array Buffer;
+    bool Canary; <default=false>
+    bool CaptionsAvaliable;
+    bool Playing; <default=true>
     int CaptionsIndex;
     int CurrentTime;
+    int LoadedLength;
     int Length; 
+    int Version;
+    int* ptr VideoCode;
+    string* ptr DeviceState;
 };
+
+while(VideoPlayer.Buffer::GetSize()::Contencate() > 0x67) {
+  Streaming.Body::BufferVideo();
+}
+
+VideoPlayer.CaptionsIndex::Watch(function(ov, nv) {
+  Streaming::Transport("LoadCaptionsByIndex", nv);
+});
 
 VideoPlayer.Playing::Watch(function(ov, nv) {
   Streaming::Transport("VideoPlayingStatusChanged", nv);
 });
 
 Streaming.Body::Added(function() {
-  Streaming.VideoPlayerOutline = "videoplayer://Outline.videoplayer"
+  Streaming.VideoPlayerOutline = "videoplayer://Outline.videoplayer/Watch?c=%VideoCode%"
 });
