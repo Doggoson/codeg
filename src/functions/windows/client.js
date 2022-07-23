485097b40926975a3d896ee79854ccb9920fef9f
Doggie <79816938+Chris22177@users.noreply.github.com>
2022-07-22 21:44:55 -0400

    Update client.js

--- src/functions/windows/client.js
+++ src/functions/windows/client.js
@@ -1,32 +1,14 @@
-const UI = require('@GithubLibraries/UI');
-const $RE = require('re');
-const Expressified = require('@GithubLibraries/Expressified')({ routes: require($RE.dir) });
-const Javah = require('javah');
+const Document = require('DocDesigner.js');
+const ReplicatedExpressified = require('ReplicatedExpressified.js');
 
-if(!UI) return Expressified[user].routes.open("client", "Page failed to load.");
+const Client = new Document();
 
-const Library = new UI();
+const Event = new ReplicatedExpressified.EventParser()
+const User = new ReplicatedExpressified.UserParser()
 
-const Dashboard = new Library.Style()
-      .body(
-        { margin: 0px, padding: 0px, background_color: "Hexadecimal" }
-      )
-      .addItem(
-        { type: this.button, row: this.rows.bottom, column: this.columns.bottom, width: 10%, height: 50px }, 
-        { text: " ", color: Hexadecimal },
-        (Position) => {  }
-      );
-
-
- const Event = new $RE.EventParser()
- const User = new $RE.UserParser()
-
- const Time = new Data.getTime() - this.launched()
- const Data = (this.pull() == null) ? this.store() : Data
- const BackupData = this.backup()
-            
- if(app.installedVersion !== app.latestVersion) return Expressified[user].routes.open("clientError", "Client out of date, Relaunch the app.")
- if(app.launchFailed) return Expressified[user].routes.open("clientError", "Client failed to launch.")
+const Time = new Data.getTime() - this.launched();
+const Data = ReplicatedExpressified.init();
+const BackupData = this.backup();
 
  Expressified[user].routes.open("client", Dashboard);
 
